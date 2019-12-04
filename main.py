4a063e41d7cb7a2ea044e29daf331fa19ae9f1ff
Alexander Ransmann <privat@cronj.de>
2019-12-04 19:35:20 +0100

    add tkinter UI and first functions

--- main.py
+++ main.py
@@ -1,36 +1,61 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 
-# Form implementation generated from reading ui file 'main.ui'
-#
-# Created by: PyQt5 UI code generator 5.13.2
-#
-# WARNING! All changes made in this file will be lost!
+import os
+from tkinter import *
+from tkinter import filedialog
 
 
-from PyQt5 import QtCore, QtGui, QtWidgets
+class App:
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+
+        self.directory = ""
+        self.list_of_files = []
+
+        self.name_label = Label(frame, text="Directory")
+        self.name_label.grid(row=0, column=0)
+        self.directory_entry = Entry(frame, width=90)
+        self.directory_entry.grid(row=0, column=1)
+        self.directory_btn = Button(frame,
+                                    text="Load",
+                                    command=self.load_directory)
+        self.directory_btn.grid(row=0, column=2)
+
+        self.name_label = Label(frame, text="Filename")
+        self.name_label.grid(row=1, column=0)
+        self.name_entry = Entry(frame, width=90)
+        self.name_entry.grid(row=1, column=1)
+        self.rename_btn = Button(frame,
+                                 text="Rename",
+                                 command=self.rename_file)
+        self.rename_btn.grid(row=1, column=2)
+
+        self.file_list = Listbox(frame,
+                                 listvariable=self.list_of_files,
+                                 selectmode=SINGLE,
+                                 height=30,
+                                 width=100)
+        self.file_list.grid(row=3, column=0, columnspan=3)
+
+    def load_directory(self):
+        self.directory = filedialog.askdirectory()
+        self.directory_entry.insert(0, self.directory)
+        self.update_file_list()
+
+    def rename_file(self):
+        print("Rename file to: " + self.name_entry.get())
+
+    def update_file_list(self):
+        if self.directory != "":
+            for _, _, self.file in os.walk(self.directory):
+                for self.element in self.file:
+                    self.file_list.insert(END, self.element)
+                    self.list_of_files.append(self.element)
 
 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(1053, 629)
-        self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
-        self.buttonBox.setGeometry(QtCore.QRect(680, 560, 341, 32))
-        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
-        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel | QtWidgets.QDialogButtonBox.Ok)
-        self.buttonBox.setObjectName("buttonBox")
-        self.listWidget = QtWidgets.QListWidget(Dialog)
-        self.listWidget.setGeometry(QtCore.QRect(10, 20, 256, 192))
-        self.listWidget.setObjectName("listWidget")
-        self.graphicsView = QtWidgets.QGraphicsView(Dialog)
-        self.graphicsView.setGeometry(QtCore.QRect(430, 90, 256, 192))
-        self.graphicsView.setObjectName("graphicsView")
-
-        self.retranslateUi(Dialog)
-        self.buttonBox.accepted.connect(Dialog.accept)
-        self.buttonBox.rejected.connect(Dialog.reject)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
-
-    def retranslateUi(self, Dialog):
-        _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+root = Tk()
+root.title("Scan rename")
+root.minsize(width=800, height=640)
+app = App(root)
+root.mainloop()
